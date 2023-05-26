ce3f027c3e270b37f373be40d5a00c3f2d1f4a25
Nebiyu <nebiyuhiybu@gmail.com>
2023-05-26 07:09:29 -0700

    getting all tasks from all users

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-  These are funcdamental modules for this task
+  These are fundamental modules for this task
 """
 import json
 import requests
@@ -10,16 +10,14 @@ from sys import argv
 
 if __name__ == ('__main__'):
     """
-    This block gets the id from command line then
-    gets the data from url using get api method
-    then displays the data.
+    This script gets all information from each
+    employee and dumps it in a .JSON file
     """
     url = "https://jsonplaceholder.typicode.com/users/{}/todos"
     response = requests.get(url[:-9])
     users = response.json()
     total_users = len(users)
     list_all = {}
-    #info_list = []
     for i in range(1, total_users + 1):
         response1 = requests.get(url[:-6].format(i))
         response2 = requests.get(url.format(i))
@@ -27,11 +25,11 @@ if __name__ == ('__main__'):
         user = response1.json()
         employee_name = user["username"]
         employee_id = user["id"]
-        total_task = sum(1 for todo in todos)
         small = []
         for todo in todos:
             small_dict = {}
-            small_dict.update({"username": employee_name, "task": todo.get("title"), "completed": todo.get("completed")})
+            small_dict.update({"username": employee_name, "task": todo.get(
+                "title"), "completed": todo.get("completed")})
             small.append(small_dict)
         list_all.update({employee_id: small})
     with open("todo_all_employees.json", "w") as f:
