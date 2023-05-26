4396accb82ef8c4ceb17291e78c4fe0cf7ecf79e
Nebiyu <nebiyuhiybu@gmail.com>
2023-05-26 07:03:43 -0700

    getting all tasks from all users

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -15,15 +15,24 @@ if __name__ == ('__main__'):
     then displays the data.
     """
     url = "https://jsonplaceholder.typicode.com/users/{}/todos"
-    employee_id = int(argv[1])
-    response = requests.get(url.format(employee_id))
-    todos = response.json()
-    response2 = requests.get(url[:-6].format(employee_id))
-    list1 = response2.json()
-    completed = sum(1 for todo in todos if todo["completed"])
-    employee_name = list1["name"]
-    total = len(todos)
-    print(f"Employee {employee_name} is done with tasks({completed}/{total}):")
-    for todo in todos:
-        if todo["completed"]:
-            print("\t {}".format(todo["title"]))
+    response = requests.get(url[:-9])
+    users = response.json()
+    total_users = len(users)
+    list_all = {}
+    #info_list = []
+    for i in range(1, total_users + 1):
+        response1 = requests.get(url[:-6].format(i))
+        response2 = requests.get(url.format(i))
+        todos = response2.json()
+        user = response1.json()
+        employee_name = user["username"]
+        employee_id = user["id"]
+        total_task = sum(1 for todo in todos)
+        small = []
+        for todo in todos:
+            small_dict = {}
+            small_dict.update({"username": employee_name, "task": todo.get("title"), "completed": todo.get("completed")})
+            small.append(small_dict)
+        list_all.update({employee_id: small})
+    with open("todo_all_employees.json", "w") as f:
+        json.dump(list_all, f)
