3de453318f8b34d9bd34550c0333bd14d052a95e
Nebiyu <nebiyuhiybu@gmail.com>
2023-05-26 04:13:18 -0700

    writing to a .CSV file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+  These are funcdamental modules for this task
+"""
+from csv import DictWriter, QUOTE_ALL
+import requests
+from sys import argv
+
+
+if __name__ == ('__main__'):
+    """
+    This block gets the id from command line then
+    gets the data from url using get api method
+    then writes it to a .CSV file.
+    """
+    url = "https://jsonplaceholder.typicode.com/users/{}/todos"
+    employee_id = int(argv[1])
+    response = requests.get(url.format(employee_id))
+    todos = response.json()
+    response2 = requests.get(url[:-6].format(employee_id))
+    list1 = response2.json()
+    employee_name = list1["username"]
+    info_list = []
+    for todo in todos:
+        info = {}
+        info.update({"user_id": employee_id, "username": employee_name, "status": todo.get("completed"),
+                          "task": todo.get("title")})
+        info_list.append(info)
+    with open(f"{employee_id}.CSV", "w") as f:
+        header = ["user_id", "username", "status", "task"]
+        writer = DictWriter(f, fieldnames = header, quoting = QUOTE_ALL)
+        writer.writerows(info_list)
