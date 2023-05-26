d716d08a7df8966ef0bc8cddd3a380878b01927c
Nebiyu <nebiyuhiybu@gmail.com>
2023-05-26 05:06:16 -0700

    dumping into json file

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""
+  These are funcdamental modules for this task
+"""
+import json
+from csv import DictWriter, QUOTE_ALL
+import requests
+from sys import argv
+
+
+if __name__ == ('__main__'):
+    """
+    This block gets the id from command line then
+    gets the data from url using get api method
+    then writes it to a .CSV file.
+    """
+    url = "https://jsonplaceholder.typicode.com/users/{}/todos"
+    employee_id = int(argv[1])
+    response = requests.get(url.format(employee_id))
+    todos = response.json()
+    response2 = requests.get(url[:-6].format(employee_id))
+    list1 = response2.json()
+    employee_name = list1["username"]
+    info_list = []
+    for todo in todos:
+        info = {}
+        info.update({"task": todo.get("title"),"completed": todo.get(
+            "completed"), "username": employee_name})
+        info_list.append(info)
+    with open(f"{employee_id}.json", "w") as f:
+        json.dump({employee_id: info_list}, f)
