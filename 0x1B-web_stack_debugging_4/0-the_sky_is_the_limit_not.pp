afb2c97fd96077bcc36586e1eaee732c7915155f
Nebiyu <nebiyuhiyabu@gmail.com>
2023-07-13 08:24:06 -0700

    the sky is not my limit

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# To increase the amount of traffic an Nginx server can handle
+
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/',
+  before  => Exec['nginx-restart']
+}
+
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
