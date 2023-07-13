e1fd1e3beb73fa3a49269bc1c88731f0dea47c9b
Nebiyu <nebiyuhiyabu@gmail.com>
2023-07-13 06:23:13 -0700

    top ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Get top ten subreddit post"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ Get top ten hot posts """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-Agent': 'Ayo User Agent 1.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        print("None")
+        return
+    posts = response.json.get('data').get('children')
+    for post on posts[:10]:
+        print(post.get('data').get('title'))
